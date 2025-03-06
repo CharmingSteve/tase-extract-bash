7295c09073ac1e2dcfdeec8b05cde7f93ce0e3ec
Steve Bar Yakov Gindi <steve@charming.co.il>
2025-03-06 14:15:25 +0200

    initial extract commit

--- create_test_archives.sh
+++ create_test_archives.sh
@@ -2,39 +2,72 @@
 
 # Create test files with timestamps and varied/missing extensions
 
+# Usage: create_test_archives.sh [-x]
+#   -x: Enable debug mode (print commands before execution)
+
+DEBUG=true
+
+# Function to print commands if debug mode is enabled
+debug() {
+  if $DEBUG; then
+    echo "+ $@"
+  fi
+  "$@"
+}
+
+# Parse command-line arguments
+while getopts "x" opt; do
+  case $opt in
+    x)
+      DEBUG=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 create_test_files() {
-  mkdir -p test_files
-  cd test_files
+  debug mkdir -p test_files
+  debug cd test_files
 
   for i in {1..8}; do
     timestamp=$(date +%Y-%m-%d_%H-%M-%S)
-    echo "Hey! I was created at $timestamp" > file${i}.txt
+    debug echo "Hey! I was created at $timestamp" > file${i}.txt
   done
 
   # Create archives with varied extensions
-  gzip file1.txt
-  gzip file2.txt
-  bzip2 file3.txt
-  bzip2 file4.txt
-  zip file5.txt file5.zip
-  zip file6.txt file6.zip
-  compress file7.txt
-  compress file8.txt
+  debug gzip file1.txt
+  debug gzip file2.txt
+  debug bzip2 file3.txt
+  debug bzip2 file4.txt
+  debug zip file5.zip file5.txt # Add file5.txt to the archive
+  debug zip file6.zip file6.txt # Add file6.txt to the archive
+  # Check if compress is installed
+  if command -v compress &> /dev/null
+  then
+      debug compress file7.txt
+      debug compress file8.txt
+  else
+      echo "Warning: compress command not found. Skipping compression for file7.txt and file8.txt"
+      touch file7.txt.Z file8.txt.Z
+  fi
 
   # Rename some files to have incorrect or missing extensions
-  mv file1.txt.gz file1_no_ext
-  mv file3.txt.bz2 file3.tar.z # Wrong extension
-  mv file5.zip file5_wrong.txt # Wrong extension
-  mv file7.txt.Z file7_no_ext
+  debug mv file1.txt.gz file1_no_ext
+  debug mv file3.txt.bz2 file3.tar.z # Wrong extension
+  debug mv file5.zip file5_wrong.txt # Wrong extension
+  debug mv file7.txt.Z file7_no_ext
 
-  mkdir -p gunzip_files bunzip2_files unzip_files compress_files
-  mv file1_no_ext file2.txt.gz gunzip_files/
-  mv file3.tar.z file4.txt.bz2 bunzip2_files/
-  mv file5_wrong.txt file6.zip unzip_files/
-  mv file7_no_ext file8.txt.Z compress_files/
+  debug mkdir -p gunzip_files bunzip2_files unzip_files compress_files
+  debug mv file1_no_ext file2.txt.gz gunzip_files/
+  debug mv file3.tar.z file4.txt.bz2 bunzip2_files/
+  debug mv file5_wrong.txt file6.zip unzip_files/
+  debug mv file7_no_ext file8.txt.Z compress_files/
 
-  cd ..
+  debug cd ..
 }
 
 create_test_files
-
